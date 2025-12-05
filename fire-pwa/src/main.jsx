39995ffe27acd86566b4885b183413f30008a0e3
JoaoSant7 <pedro_romano7@proton.me>
2025-12-05 12:14:06 -0300

    ajustado .gitignore para incluir yarn.lock e ajustado caminho do css no main.jsx

--- fire-pwa/src/main.jsx
+++ fire-pwa/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './App.css'
+import './styles/App.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
 // O Service Worker é registrado automaticamente pelo vite-plugin-pwa
