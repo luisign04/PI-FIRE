538111e87e50e3f9667840d84c08190bfe7299d6
JoaoSant7 <pedro_romano7@proton.me>
2025-12-06 14:07:26 -0300

    ajustado const navigate de OcorrenciaSucesso

--- fire-pwa/src/pages/OcorrenciaSucesso.jsx
+++ fire-pwa/src/pages/OcorrenciaSucesso.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/OcorrenciaSucesso.css";
 
-const OcorrenciaRegistradaScreen = () => {
+const OcorrenciaSucesso = () => {
   const navigate = useNavigate();
 
   const handleInicio = () => {
