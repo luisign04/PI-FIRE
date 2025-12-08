68559fee58a89c29132835fb741dcefd6649016a
Gustavo Nemésio Barreto <166542547+gustavosimio@users.noreply.github.com>
2025-12-07 21:56:26 -0300

    Delete backend directory

--- backend/src/database/connection.ts
+++
@@ -1,7 +0,0 @@
-import knex from 'knex';
-const config = require('../../knexfile');
-
-const environment = process.env.NODE_ENV || 'development';
-const db = knex(config[environment]);
-
-export default db;
